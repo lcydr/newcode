d177530b7fde5cad18727d3efff0c6ea8513794e
wlt <734490921@qq.com>
2022-06-28 20:37:08 +0800

    init project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template lang="">
+  <div>
+    <a v-bind:href="url">点击去百度</a>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import imgObj from './assets/logo.png'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      url: 'http://www.baidu.com',
+      imgUrl:
+        'http://yun.itheima.com/Upload/./Images/20210412/60741c11ab77b.jpg',
+      localImg: imgObj,
+    }
+  },
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style lang=""></style>
